594b171cb8e66caae5289bed9d4fd43ff0f20589
Chris Hsu <yaostyle@gmail.com>
2019-09-06 10:39:24 -0700

    Adds align example

---
+++ 2019/align_example/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Align Demo',
+        home: Scaffold(
+          body: AlignExamples(),
+        ));
+  }
+}
+
+class AlignExamples extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Flexible(
+          flex: 1,
+          child: Container(
+            color: Colors.black26,
+            child: Align(
+              alignment: Alignment(0, 0),
+              child: Text('Centered!'),
+            ),
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: Stack(
+            children: <Widget>[
+              Positioned.fill(
+                child: Container(
+                  color: Colors.black12,
+                ),
+              ),
+              Align(
+                alignment: Alignment(-1, -1),
+                child: Text('Top Left!'),
+              ),
+              Align(
+                alignment: Alignment(0, 0),
+                child: Text('Centered!'),
+              ),
+              Align(
+                alignment: Alignment(1, 1),
+                child: Text('Bottom Right!'),
+              ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Text('TopCenter!'),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Text('BottomCenter!'),
+              ),
+            ],
+          ),
+        ),
+        Flexible(
+          flex: 2,
+          child: Container(
+            color: Colors.black12,
+            child: Align(
+              alignment: Alignment(.5, .5),
+              child: Text('75% in'),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
